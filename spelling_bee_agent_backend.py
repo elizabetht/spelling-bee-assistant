f517d0a3391d614b2a2ceebf65469ed400bc6b42
Elizabeth Thomas <email2eliza@gmail.com>
2026-02-15 16:25:56 -0600

    Update without cache

--- spelling_bee_agent_backend.py
+++ spelling_bee_agent_backend.py
@@ -362,7 +362,7 @@ if PIPECAT_AVAILABLE:
 
         tts = ElevenLabsTTSService(
             api_key=os.getenv("ELEVENLABS_API_KEY"),
-            voice_id=os.getenv("ELEVENLABS_TTS_VOICE_ID", "21m00Tcm4TlvDq8ikWAM"),
+            voice_id=os.getenv("ELEVENLABS_TTS_VOICE_ID", "9BWtsMINqrJLrRacOk9x"),
             model="eleven_turbo_v2_5",
         )
 
